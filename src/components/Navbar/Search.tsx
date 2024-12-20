8164174c74dc901d5e40838fd47e04595d9f2ace
Duke Lee <minhduc8a2.1@gmail.com>
2024-12-20 22:57:59 +0700

    add search post on navbar

--- src/components/Navbar/Search.tsx
+++ src/components/Navbar/Search.tsx
@@ -4,9 +4,12 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  Tab,
+  Tabs,
 } from "@nextui-org/react"
+import { useRouter } from "next/navigation"
 
-import React from "react"
+import React, { useState } from "react"
 import { IoSearchSharp } from "react-icons/io5"
 
 export default function Search() {
@@ -18,29 +21,57 @@ export default function Search() {
         </Button>
       </PopoverTrigger>
       <PopoverContent className="min-w-[300px]">
-        {(titleProps) => (
-          <div className="px-2 py-3 w-full">
-            <p className="text-small font-bold text-foreground" {...titleProps}>
-              Tìm kiếm bài viết, girl xinh
-            </p>
-            <div className="mt-2 flex flex-col gap-2 w-full">
-              <Input
-                classNames={{
-                  base: "max-w-full sm:max-w-[15rem] h-10 hidden sm:block",
-                  mainWrapper: "h-full",
-                  input: "text-small",
-                  inputWrapper:
-                    "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                }}
-                placeholder="Type to search..."
-                size="sm"
-                startContent={<IoSearchSharp />}
-                type="search"
-              />
-            </div>
-          </div>
-        )}
+        <div className="px-2 py-3 w-full">
+          <Tabs aria-label="Options">
+            <Tab key="posts" title="Bài viết">
+              <Form placeholder="Tìm kiếm bài viết" url="/posts" />
+            </Tab>
+            <Tab key="girls" title="Girl xinh">
+              <Form placeholder="Tìm kiếm girl xinh" url="/topics" />
+            </Tab>
+          </Tabs>
+        </div>
       </PopoverContent>
     </Popover>
+  )
+}
+function Form({
+  placeholder,
+  url,
+}: Readonly<{ placeholder: string; url: string }>) {
+  const [inputValue, setInputValue] = useState("")
+  const router = useRouter()
+  function onSearch(clear: boolean = false) {
+    router.push(`${url}?search=${clear ? "" : inputValue}`)
+  }
+  return (
+    <form
+      className="mt-2 flex flex-col gap-2 w-full"
+      onSubmit={(e) => {
+        e.preventDefault()
+        onSearch()
+      }}
+    >
+      <Input
+        value={inputValue}
+        onValueChange={(value) => {
+          setInputValue(value)
+          if (value == "") {
+            onSearch(true)
+          }
+        }}
+        classNames={{
+          base: "max-w-full sm:max-w-[15rem] h-10 hidden sm:block",
+          mainWrapper: "h-full",
+          input: "text-small",
+          inputWrapper:
+            "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+        }}
+        placeholder={placeholder}
+        size="sm"
+        startContent={<IoSearchSharp />}
+        type="search"
+      />
+    </form>
   )
 }
