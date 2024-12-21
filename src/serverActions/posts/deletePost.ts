459555101cbb15350cff6f9396c8a9f049ad2d85
Duke Lee <minhduc8a2.1@gmail.com>
2024-12-21 21:34:33 +0700

    redirect when delete post already

--- src/serverActions/posts/deletePost.ts
+++ src/serverActions/posts/deletePost.ts
@@ -5,20 +5,21 @@ import { deleteMediaByURLs } from "@/services/media/mediaService"
 import { deletePost as deletePostService, getPost } from "@/services/posts"
 import { PostType } from "@/types/posts.types"
 import mongoose from "mongoose"
+import { redirect } from "next/navigation"
 
-export async function deletePost(param:string) {
+export async function deletePost(param: string) {
   await protectUpdateContentPage()
- 
+
   let aborted = false
   await dbConnect()
   const DBsession = await mongoose.startSession()
   try {
     DBsession.startTransaction()
-    const post:PostType = await getPost({param},true)
+    const post: PostType = await getPost({ param }, true)
     //create postcreatePost
-    const result = await deletePostService(param,false,DBsession)
-    if(result.deletedCount>0){
-        deleteMediaByURLs(post.body.map((bodyItem) => bodyItem.url))
+    const result = await deletePostService(param, false, DBsession)
+    if (result.deletedCount > 0) {
+      deleteMediaByURLs(post.body.map((bodyItem) => bodyItem.url))
     }
     //commit
     await DBsession.commitTransaction()
@@ -30,6 +31,7 @@ export async function deletePost(param:string) {
   } finally {
     DBsession.endSession()
   }
-  if (aborted) return { message: "Có lỗi xảy ra! Không thể xóa bài viết ngay lúc này!" }
-  return { message: "Xóa bài viết thành công" }
+  if (aborted)
+    return { message: "Có lỗi xảy ra! Không thể xóa bài viết ngay lúc này!" }
+  return redirect("/admin/posts")
 }
